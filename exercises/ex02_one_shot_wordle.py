679e88c60fe8bae1aaa59896143337c8b580f605
Zheyuan Liu <zheyuan@ad.unc.edu>
2023-09-20 11:03:58 -0400

    Progress on EX02

---
+++ exercises/ex02_one_shot_wordle.py
@@ -0,0 +1,37 @@
+"""EX01_one_shot_wordle"""
+__author__ = "730521715"
+
+WHITE_BOX: str = "\U00002B1C"
+GREEN_BOX: str = "\U0001F7E9"
+YELLOW_BOX: str = "\U0001F7E8"
+
+secret = "python"
+guess = input(f"What is your {len(secret)}-letter guess? ")
+
+while len(guess) != len(secret):
+    guess = input(f"That was not {len(secret)} letters! Try again: ")
+
+result_string = ""
+for idx in range(len(secret)):
+    if guess[idx] == secret[idx]:
+        result_string += GREEN_BOX
+    else:
+        # Check if letter exists in other positions
+        found = False
+        alt_idx = 0
+        while not found and alt_idx < len(secret):
+            if guess[idx] == secret[alt_idx] and idx != alt_idx:
+                found = True
+            alt_idx += 1
+
+        if found:
+            result_string += YELLOW_BOX
+        else:
+            result_string += WHITE_BOX
+
+print(result_string)
+
+if guess == secret:
+    print("Woo! You got it!")
+else:
+    print("Not quite. Play again soon!")
