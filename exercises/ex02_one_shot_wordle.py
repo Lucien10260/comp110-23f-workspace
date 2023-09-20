f88aff5855d9405c683668840f1b4ba4c233e723
Zheyuan Liu <zheyuan@ad.unc.edu>
2023-09-20 18:20:39 -0400

    ex02

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -1,15 +1,12 @@
 """EX01_one_shot_wordle."""
 __author__ = "730521715"
 secret_word = "python"
-
 guess = input(f"What is your {len(secret_word)}-letter guess? ")
-
 while len(guess) != len(secret_word):
     guess = input(f"That was not {len(secret_word)} letters! Try again: ")
 WHITE_BOX = "\U00002B1C"
 GREEN_BOX = "\U0001F7E9"
 YELLOW_BOX = "\U0001F7E8"
-
 result_emoji = ""
 index = 0
 while index < len(secret_word):
@@ -29,9 +26,7 @@ while index < len(secret_word):
                 result_emoji = result_emoji[:index] + YELLOW_BOX + result_emoji[index + 1:]
             alt_index += 1
     index += 1
-
 print(result_emoji)
-
 if result_emoji == GREEN_BOX * len(secret_word):
     print("Woo! You got it!")
 else:
