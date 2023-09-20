7c7457aea63aa0834d1fa32797fd33eb9f1bb8b9
Zheyuan Liu <zheyuan@ad.unc.edu>
2023-09-20 18:20:10 -0400

    ex02

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -1,31 +1,38 @@
 """EX01_one_shot_wordle."""
 __author__ = "730521715"
-WHITE_BOX: str = "\U00002B1C"
-GREEN_BOX: str = "\U0001F7E9"
-YELLOW_BOX: str = "\U0001F7E8"
-secret = "bullet"
-guess = input(f"What is your {len(secret)}-letter guess? ")
-while len(guess) != len(secret):
-    guess = input(f"That was not {len(secret)} letters! Try again: ")
-result_string = ""
-idx = 0
-while idx < len(secret):
-    if guess[idx] == secret[idx]:
-        result_string += GREEN_BOX
+secret_word = "python"
+
+guess = input(f"What is your {len(secret_word)}-letter guess? ")
+
+while len(guess) != len(secret_word):
+    guess = input(f"That was not {len(secret_word)} letters! Try again: ")
+WHITE_BOX = "\U00002B1C"
+GREEN_BOX = "\U0001F7E9"
+YELLOW_BOX = "\U0001F7E8"
+
+result_emoji = ""
+index = 0
+while index < len(secret_word):
+    if guess[index] == secret_word[index]:
+        result_emoji += GREEN_BOX
     else:
+        result_emoji += WHITE_BOX
+    index += 1
+index = 0
+while index < len(secret_word):
+    if guess[index] != secret_word[index]:
         found = False
-        alt_idx = 0
-        while not found and alt_idx < len(secret):
-            if guess[idx] == secret[alt_idx] and idx != alt_idx:
+        alt_index = 0
+        while not found and alt_index < len(secret_word):
+            if guess[index] == secret_word[alt_index] and index != alt_index:
                 found = True
-            alt_idx += 1
-        if found:
-            result_string += YELLOW_BOX
-        else:
-            result_string += WHITE_BOX
-    idx += 1
-print(result_string)
-if guess == secret:
+                result_emoji = result_emoji[:index] + YELLOW_BOX + result_emoji[index + 1:]
+            alt_index += 1
+    index += 1
+
+print(result_emoji)
+
+if result_emoji == GREEN_BOX * len(secret_word):
     print("Woo! You got it!")
 else:
     print("Not quite. Play again soon!")
