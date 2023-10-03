66ce4d8edbe8d8d61a3cecaa977b9750440c6854
Zheyuan Liu <zheyuan@ad.unc.edu>
2023-10-02 22:23:06 -0400

    ex03

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -3,33 +3,34 @@ __author__ = "730521715"
 WHITE_BOX = "\U00002B1C"
 GREEN_BOX = "\U0001F7E9"
 YELLOW_BOX = "\U0001F7E8"
-def contains_char(a: str, b: str) -> bool:
+def contains_char(word: str, char_to_check: str) -> bool:
     """
-    Check if a single character (b) exists in the provided string.
+    Check if a single character (char_to_check) exists in the provided string.
     Args:
-    - a: The string we want to check for the presence of the b.
-    - b: A single character string we want to check the presence of.
+    - word: The string we want to check for the presence of the char_to_check.
+    - char_to_check: A single character string we want to check the presence of.
     Returns:
-    - bool: True if the b is found in a, False otherwise.
+    - bool: True if the char_to_check is found in word, False otherwise.
     """
-    assert len(b) == 1, "b must be a single character"
-    for char in a:
-        if char == b:
+    assert len(char_to_check) == 1, "char_to_check must be a single character"
+    
+    for char in word:
+        if char == char_to_check:
             return True
     return False
 
 def emojified(guess: str, secret: str) -> str:
     assert len(guess) == len(secret)
-
-    result = []
+    result = "" 
+    # Compare each character in guess to corresponding character in secret
     for i in range(len(guess)):
         if guess[i] == secret[i]:
-            result.append(GREEN_BOX)
+            result += GREEN_BOX
         elif contains_char(secret, guess[i]):
-            result.append(YELLOW_BOX)
+            result += YELLOW_BOX
         else:
-            result.append(WHITE_BOX)
-    return ''.join(result)
+            result += WHITE_BOX
+    return result
 
 def input_guess(expected_length: int) -> str:
     guess = input(f"Enter a {expected_length} character word: ")
@@ -39,17 +40,17 @@ def input_guess(expected_length: int) -> str:
 
 def main() -> None:
     secret_word = "codes"
-    max_turns = 6
+    max_turns = len(secret_word) + 1
     turns_taken = 0
     has_won = False
-
     while turns_taken < max_turns and not has_won:
         turns_taken += 1
         print(f"=== Turn {turns_taken}/{max_turns} ===")
+        
         guess = input_guess(len(secret_word))
         emoji_result = emojified(guess, secret_word)
         print(emoji_result)
-
+        
         if guess == secret_word:
             has_won = True
 
