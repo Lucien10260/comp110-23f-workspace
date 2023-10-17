555fe3bb91050278de2edce6f5778f5ec3a92917
Zheyuan Liu <zheyuan@ad.unc.edu>
2023-10-17 11:48:48 -0400

    1

--- lessons/sum.py
+++ lessons/sum.py
@@ -53,28 +53,3 @@ def f_range_sum(vals: list[float]) -> float:
     for i in range(len(vals)):
         total += vals[i]
     return total
-"""Summing the elements of a list using different loops"""
-__author__ = "730521715"
-
-# Part 1: w_sum() function
-def w_sum(vals: list[float]) -> float:
-    total = 0.0
-    i = 0
-    while i < len(vals):
-        total += vals[i]
-        i += 1
-    return total
-
-# Part 2: f_sum() function
-def f_sum(vals: list[float]) -> float:
-    total = 0.0
-    for val in vals:
-        total += val
-    return total
-
-# Part 3: f_range_sum() function
-def f_range_sum(vals: list[float]) -> float:
-    total = 0.0
-    for i in range(len(vals)):
-        total += vals[i]
-    return total
