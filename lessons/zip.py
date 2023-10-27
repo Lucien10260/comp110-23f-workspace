7dc255db4e62a312b4c7d4fe55db63ca33e10b7e
Zheyuan Liu <zheyuan@ad.unc.edu>
2023-10-27 19:40:10 -0400

    1

--- lessons/zip.py
+++ lessons/zip.py
@@ -1,22 +1,21 @@
 """Combining two lists into a dictionary."""
-
-__author__ = "YOUR_PID"
+__author__ = "730521715"
 
 from typing import List, Dict
 
 
 def zip(strings: List[str], numbers: List[int]) -> Dict[str, int]:
-    """Combine two lists into a dictionary.
-    
+    """
+    Combine two lists into a dictionary.
+
     Parameters:
-    strings (List[str]): A list of strings to be used as dictionary keys.
-    numbers (List[int]): A list of integers to be used as dictionary values.
+    - strings (List[str]): A list of strings to be used as dictionary keys.
+    - numbers (List[int]): A list of integers to be used as dictionary values.
 
     Returns:
     Dict[str, int]: A dictionary with strings as keys and numbers as values.
     If the input lists are of different lengths or empty, it returns an empty dictionary.
     """
-    
     if len(strings) != len(numbers) or not strings or not numbers:
         return {}
 
