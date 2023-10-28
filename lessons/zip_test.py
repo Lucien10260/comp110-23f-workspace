b6fc90877fe08f52df061c2f3667f01fe008dfb5
Zheyuan Liu <zheyuan@ad.unc.edu>
2023-10-27 23:02:08 -0400

    1

--- lessons/zip_test.py
+++ lessons/zip_test.py
@@ -4,11 +4,13 @@ __author__ = "730521715"
 
 from lessons.zip import zip
 
+
 def test_zip_mismatched_length_lists() -> None:
     """Tests the case where the two lists are of different lengths."""
     strings: list[str] = ["a", "b"]
     integers: list[int] = [1]
     assert zip(strings, integers) == {}
+
 
 def test_zip_normal_case() -> None:
     """Tests a normal use case where two lists of the same length are combined."""
@@ -16,11 +18,13 @@ def test_zip_normal_case() -> None:
     integers: list[int] = [1, 2, 3]
     assert zip(strings, integers) == {"a": 1, "b": 2, "c": 3}
 
+
 def test_zip_empty_lists() -> None:
     """Tests the edge case where both lists are empty."""
     strings: list[str] = []
     integers: list[int] = []
     assert zip(strings, integers) == {}
+
 
 if __name__ == "__main__":
     test_zip_mismatched_length_lists()
