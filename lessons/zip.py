862ef660e741a06ac1abb7f1f445d6bd9cb5a181
Zheyuan Liu <zheyuan@ad.unc.edu>
2023-10-27 20:20:49 -0400

    1

--- lessons/zip.py
+++ lessons/zip.py
@@ -3,25 +3,16 @@
 __author__ = "730521715"
 
 from typing import List, Dict
-from builtins import zip as _zip
-
 
 def zip(keys: List[str], values: List[int]) -> Dict[str, int]:
     """Combine two lists into a dictionary.
 
     If the input lists are of different lengths or are empty, it returns an empty dictionary.
-    
-    Args:
-    - keys: List of strings to be used as dictionary keys.
-    - values: List of integers to be used as dictionary values.
-
-    Returns:
-    Dictionary combining the provided keys and values.
     """
     if len(keys) != len(values) or not keys or not values:
         return {}
     
-    return dict(_zip(keys, values))
+    return dict(zip(keys, values))
 
 
 if __name__ == "__main__":
