04480a2cbb11c69e8112c445d99d5f243b10759f
Zheyuan Liu <zheyuan@ad.unc.edu>
2023-10-27 22:56:17 -0400

    1

--- lessons/zip_test.py
+++ lessons/zip_test.py
@@ -1,24 +1,25 @@
 """Test my zip function."""
-__author__ = "730521715"
+
+__author__ = "YOUR_PID_HERE"
 
 from lessons.zip import zip
 
-def test_zip_mismatched_length_lists():
+def test_zip_mismatched_length_lists() -> None:
     """Tests the case where the two lists are of different lengths."""
-    strings = ["a", "b"]
-    integers = [1]
+    strings: List[str] = ["a", "b"]
+    integers: List[int] = [1]
     assert zip(strings, integers) == {}
 
-def test_zip_normal_case():
+def test_zip_normal_case() -> None:
     """Tests a normal use case where two lists of the same length are combined."""
-    strings = ["a", "b", "c"]
-    integers = [1, 2, 3]
+    strings: List[str] = ["a", "b", "c"]
+    integers: List[int] = [1, 2, 3]
     assert zip(strings, integers) == {"a": 1, "b": 2, "c": 3}
 
-def test_zip_empty_lists():
+def test_zip_empty_lists() -> None:
     """Tests the edge case where both lists are empty."""
-    strings = []
-    integers = []
+    strings: List[str] = []
+    integers: List[int] = []
     assert zip(strings, integers) == {}
 
 if __name__ == "__main__":
