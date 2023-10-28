23e4ebf702a250eef90626cc2b27e2991c926f60
Zheyuan Liu <zheyuan@ad.unc.edu>
2023-10-27 22:49:07 -0400

    1

--- lessons/zip.py
+++ lessons/zip.py
@@ -2,27 +2,13 @@
 
 __author__ = "730521715"
 
-from typing import List, Dict
-from builtins import zip as _zip
 
-
-def zip(keys: List[str], values: List[int]) -> Dict[str, int]:
-    """Combine two lists into a dictionary.
-
-    If the input lists are of different lengths or are empty, it returns an empty dictionary.
-    
-    Args:
-    - keys: List of strings to be used as dictionary keys.
-    - values: List of integers to be used as dictionary values.
-
-    Returns:
-    Dictionary combining the provided keys and values.
-    """
-    if len(keys) != len(values) or not keys or not values:
+def zip(strings: list[str], integers: list[int]) -> dict[str, int]:
+    """Combine two lists into a dictionary. The keys from the the first list and values are corresponding items from the second list"""
+    if len(strings) != len(integers) or not strings or not integers:
         return {}
-    
-    return dict(_zip(keys, values))
 
-
-if __name__ == "__main__":
-    print(zip(["Happy", "Tuesday", "Today"], [1, 2, 3]))  # Expected output: {"Happy": 1, "Tuesday": 2, "Today": 3}
+    final_dic = {}
+    for i in range(len(strings)):
+        final_dic[strings[i]] = integers[i]
+    return final_dic
