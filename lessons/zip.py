cbe3c1798a9022aebc293829869289a9ef443008
Zheyuan Liu <zheyuan@ad.unc.edu>
2023-10-27 20:11:53 -0400

    1

--- lessons/zip.py
+++ lessons/zip.py
@@ -1,13 +1,11 @@
-"""Combining two lists into a dictionary."""
-__author__ = "730521715"
+"""Combining two lists into a dictionary"""
+
+__author__ = "=730521715"
 
 from typing import List, Dict
 
-def zip(strings: List[str], integers: List[int]) -> Dict[str, int]:
-    if len(strings) != len(integers) or not strings or not integers:
+def zip(keys: List[str], values: List[int]) -> Dict[str, int]:
+    if len(keys) != len(values) or not keys or not values:
         return {}
     
-    return dict(zip(strings, integers))
-
-# Example usage:
-print(zip(["Happy", "Tuesday"], [1, 2]))  # {"Happy": 1, "Tuesday": 2}
+    return dict(zip(keys, values))
