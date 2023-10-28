25d53ba78d73393e9826bd824505c535e459ab9b
Zheyuan Liu <zheyuan@ad.unc.edu>
2023-10-27 22:51:17 -0400

    1

--- lessons/zip.py
+++ lessons/zip.py
@@ -4,7 +4,7 @@ __author__ = "730521715"
 
 
 def zip(strings: list[str], integers: list[int]) -> dict[str, int]:
-    """Combine two lists into a dictionary. The keys from the the first list and values are corresponding items from the second list"""
+    """Combine two lists into a dictionary. The keys from the the first list and values are corresponding items from the second list."""
     if len(strings) != len(integers) or not strings or not integers:
         return {}
 
