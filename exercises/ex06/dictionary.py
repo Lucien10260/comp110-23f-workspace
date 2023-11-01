4c689ecbff1e9a051d1ac83ad28ccd93d3a73689
Zheyuan Liu <zheyuan@ad.unc.edu>
2023-11-01 18:55:43 -0400

    1

--- exercises/ex06/dictionary.py
+++ exercises/ex06/dictionary.py
@@ -1,28 +1,29 @@
-"""dictionary."""
+"""Dictionary."""
 
 __author__ = "730521715"
 
-def invert(input_dict: dict[str, str]) -> dict[str, str]:
-    """invert the keys and values of a dictionary.
 
-    args:
-        input_dict: a dictionary to invert.
+def invert(input_dict: Dict[str, str]) -> Dict[str, str]:
+    """Invert the keys and values of a dictionary.
 
-    returns:
-        a dictionary with keys and values inverted.
+    Args:
+        input_dict: A dictionary to invert.
 
-    raises:
-        keyerror: if a value is not unique, i.e., it appears more than once.
+    Returns:
+        A dictionary with keys and values inverted.
+
+    Raises:
+        KeyError: If a value is not unique, i.e., it appears more than once.
     """
-    inverted_dict = {}
+    inverted_dict: Dict[str, str] = {}
     for key, value in input_dict.items():
         if value in inverted_dict:
-            raise keyerror(f"value {value} is not unique.")
+            raise KeyError(f"Value {value} is not unique.")
         inverted_dict[value] = key
-    return inverted_dict  # make sure to return the inverted dictionary
+    return inverted_dict
 
 
-def favorite_color(names_to_colors: dict[str, str]) -> str:
+def favorite_color(names_to_colors: Dict[str, str]) -> str:
     """Find the most frequent color in the dictionary.
 
     Args:
@@ -31,78 +32,61 @@ def favorite_color(names_to_colors: dict[str, str]) -> str:
     Returns:
         The color that appears most frequently in the dictionary.
     """
-    # Initialize a dictionary to count color occurrences.
-    color_counts = {}
-
-    # Iterate over all colors and count them.
+    color_counts: Dict[str, int] = {}
     for color in names_to_colors.values():
-        if color in color_counts:
-            color_counts[color] += 1
-        else:
-            color_counts[color] = 1
-
-    # Find the color that appears most frequently.
-    # If there is a tie, it will return the first one that appears most frequently.
+        color_counts[color] = color_counts.get(color, 0) + 1
     most_common_color = max(color_counts, key=lambda k: (color_counts[k], -list(names_to_colors.values()).index(k)))
-    
     return most_common_color
 
 
-def count(values_list: list[str]) -> dict[str, int]:
-    """count the occurrences of each string in a list.
+def count(values_list: List[str]) -> Dict[str, int]:
+    """Count the occurrences of each string in a list.
 
-    args:
-        values_list: a list of strings to count occurrences in.
+    Args:
+        values_list: A list of strings to count occurrences in.
 
-    returns:
-        a dictionary with keys as the unique strings from the list and values as the counts.
+    Returns:
+        A dictionary with keys as the unique strings from the list and values as the counts.
     """
-    count_dict = {}
+    count_dict: Dict[str, int] = {}
     for item in values_list:
-        if item in count_dict:
-            count_dict[item] += 1
-        else:
-            count_dict[item] = 1
+        count_dict[item] = count_dict.get(item, 0) + 1
     return count_dict
 
-def alphabetizer(words_list: list[str]) -> dict[str, list[str]]:
-    """organize words by their first letter.
 
-    args:
-        words_list: a list of words to categorize.
+def alphabetizer(words_list: List[str]) -> Dict[str, List[str]]:
+    """Organize words by their first letter.
 
-    returns:
-        a dictionary where each key is a lowercase letter and each value is a list
+    Args:
+        words_list: A list of words to categorize.
+
+    Returns:
+        A dictionary where each key is a lowercase letter and each value is a list
         of words that start with that letter, sorted in the order they appeared in the input list.
     """
-    alphabetized_dict = {}
+    alphabetized_dict: Dict[str, List[str]] = {}
     for word in words_list:
-        # convert the first letter of the word to lowercase
         first_letter = word[0].lower()
-        # if the first letter is not already a key in the dictionary, create a new entry
         if first_letter not in alphabetized_dict:
             alphabetized_dict[first_letter] = []
-        # append the current word to the list that corresponds to the first letter
         alphabetized_dict[first_letter].append(word)
     return alphabetized_dict
 
-def update_attendance(attendance_dict: dict[str, list[str]], day: str, student: str) -> dict[str, list[str]]:
-    """
-    update the attendance record for a given day with the student's name.
 
-    args:
-        attendance_dict: a dictionary with days as keys and lists of students as values.
-        day: the day of the week to update attendance for.
-        student: the name of the student to add to the attendance.
+def update_attendance(attendance_dict: Dict[str, List[str]], day: str, student: str) -> Dict[str, List[str]]:
+    """Update the attendance record for a given day with the student's name.
 
-    returns:
-        the updated dictionary with the student added to the attendance for the specified day.
+    Args:
+        attendance_dict: A dictionary with days as keys and lists of students as values.
+        day: The day of the week to update attendance for.
+        student: The name of the student to add to the attendance.
+
+    Returns:
+        The updated dictionary with the student added to the attendance for the specified day.
     """
-    # if the day is already in the dictionary, append the student to that day's list
     if day in attendance_dict:
-        # ensuring no duplicates if the student is already in the list for that day
         if student not in attendance_dict[day]:
             attendance_dict[day].append(student)
     else:
-        # if the day is not in the dictionary, create a new entry with the student in a new list
         attendance_dict[day] = [student]
+    return attendance_dict
