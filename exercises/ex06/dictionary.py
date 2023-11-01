45d570036d6ee4c799720be2b97b8f0821bf3d49
Zheyuan Liu <zheyuan@ad.unc.edu>
2023-11-01 18:33:14 -0400

    1

--- exercises/ex06/dictionary.py
+++ exercises/ex06/dictionary.py
@@ -1,90 +1,118 @@
-"Dictionary exercise."
+"""Dictionary."""
 
 __author__ = "730521715"
 
 def invert(input_dict: dict[str, str]) -> dict[str, str]:
+    """Invert the keys and values of a dictionary.
+
+    Args:
+        input_dict: A dictionary to invert.
+
+    Returns:
+        A dictionary with keys and values inverted.
+
+    Raises:
+        KeyError: If a value is not unique, i.e., it appears more than once.
+    """
     inverted_dict = {}
     for key, value in input_dict.items():
         if value in inverted_dict:
-            raise KeyError(f"Duplicate key found after inversion: {value}")
+            raise KeyError(f"Value {value} is not unique.")
         inverted_dict[value] = key
-    return inverted_dict -> dict[str,str]
+    return inverted_dict
 
-def favorite_color(colors_dict):
-    # This dictionary will hold the count of each color
-    color_count = {}
+def favorite_colors(names_to_colors: dict[str, str]) -> str:
+    color_counts = {}
+    for name, color in names_to_colors.items():
+        if color not in color_counts:
+            color_counts[color] = {'count': 0, 'first_index': len(color_counts)}
+        color_counts[color]['count'] += 1
 
-    # This will keep track of the order in which each color appears
-    color_order = []
+    # Sort colors by the highest count and then by the earliest appearance.
+    sorted_colors = sorted(color_counts.items(), 
+                           key=lambda item: (-item[1]['count'], item[1]['first_index']))
+    
+    # Return the color with the highest count that appeared first.
+    return sorted_colors[0][0]
 
-    # Iterate over each name and color in the input dictionary
-    for name, color in colors_dict.items():
-        # If the color is already in the count dictionary, increment its count
-        if color in color_count:
-            color_count[color] += 1
+
+def count(values_list: list[str]) -> dict[str, int]:
+    """Count the occurrences of each string in a list.
+
+    Args:
+        values_list: A list of strings to count occurrences in.
+
+    Returns:
+        A dictionary with keys as the unique strings from the list and values as the counts.
+    """
+    count_dict = {}
+    for item in values_list:
+        if item in count_dict:
+            count_dict[item] += 1
         else:
-            # Otherwise, add the color to the count dictionary and set its count to 1
-            color_count[color] = 1
-            # Also, record the color's first appearance order
-            color_order.append(color)
+            count_dict[item] = 1
+    return count_dict
 
-    # Find the maximum count of any color
-    max_count = max(color_count.values())
-    
-    # Filter the colors that have the maximum count
-    max_colors = [color for color, count in color_count.items() if count == max_count]
 
-    # Iterate through the color order list and return the first color that has the maximum count
-    for color in color_order:
-        if color in max_colors:
-            return color
+def alphabetizer(words_list: list[str]) -> dict[str, list[str]]:
+    """Organize words by their first letter.
 
-    # If there are no colors, we can return a default value or raise an error
-    raise ValueError("No colors provided")
+    Args:
+        words_list: A list of words to categorize.
 
-def main():
-    # Sample usage of the favorite_color function
-    print(favorite_color({"Marc": "yellow", "Ezri": "blue", "Kris": "blue"}))
-
-# This allows the module to be run directly from the command line
-if __name__ == "__main__":
-    main()
-
-def count(values):
-# Establish an empty dictionary to store the result
-result = {}
-    
-# Loop through each item in the input list
-for item in values:
-    # Check to see if the item is already a key in the result dictionary
-    if item in result:
-        # Increase the count associated with that key by 1
-        result[item] += 1
-    else:
-        # Assign an initial count of 1 to that key in the result dictionary
-        result[item] = 1
-            
-    # Return the resulting dictionary
-    return result
-
-def alphabetizer(words):
-    categorized_words = {}
-    for word in words:
+    Returns:
+        A dictionary where each key is a lowercase letter and each value is a list
+        of words that start with that letter, sorted in the order they appeared in the input list.
+    """
+    alphabetized_dict = {}
+    for word in words_list:
         # Convert the first letter of the word to lowercase
         first_letter = word[0].lower()
-        # Append the word to the appropriate list, creating a new list if necessary
-        if first_letter not in categorized_words:
-            categorized_words[first_letter] = [word]
-        else:
-            categorized_words[first_letter].append(word)
-    return categorized_words
+        # If the first letter is not already a key in the dictionary, create a new entry
+        if first_letter not in alphabetized_dict:
+            alphabetized_dict[first_letter] = []
+        # Append the current word to the list that corresponds to the first letter
+        alphabetized_dict[first_letter].append(word)
+    return alphabetized_dict
 
-def update_attendance(attendance, day, student):
-    # If the day is not in the dictionary, add it with a new list containing the student
-    if day not in attendance:
-        attendance[day] = [student]
+def update_attendance(attendance_dict: dict[str, list[str]], day: str, student: str) -> dict[str, list[str]]:
+    """
+    Update the attendance record for a given day with the student's name.
+
+    Args:
+        attendance_dict: A dictionary with days as keys and lists of students as values.
+        day: The day of the week to update attendance for.
+        student: The name of the student to add to the attendance.
+
+    Returns:
+        The updated dictionary with the student added to the attendance for the specified day.
+    """
+    # If the day is already in the dictionary, append the student to that day's list
+    if day in attendance_dict:
+        # Ensuring no duplicates if the student is already in the list for that day
+        if student not in attendance_dict[day]:
+            attendance_dict[day].append(student)
     else:
-        # Add the student to the existing list for the day, if they're not already listed
-        if student not in attendance[day]:
-            attendance[day].append(student)
-    return attendance
+        # If the day is not in the dictionary, create a new entry with the student in a new list
+        attendance_dict[day] = [student]
+    return attendance_dicttdef update_attendance(attendance_dict: dict[str, list[str]], day: str, student: str) -> dict[str, list[str]]:
+    """
+    Update the attendance record for a given day with the student's name.
+
+    Args:
+        attendance_dict: A dictionary with days as keys and lists of students as values.
+        day: The day of the week to update attendance for.
+        student: The name of the student to add to the attendance.
+
+    Returns:
+        The updated dictionary with the student added to the attendance for the specified day.
+    """
+    # If the day is already in the dictionary, append the student to that day's list
+    if day in attendance_dict:
+        # Ensuring no duplicates if the student is already in the list for that day
+        if student not in attendance_dict[day]:
+            attendance_dict[day].append(student)
+    else:
+        # If the day is not in the dictionary, create a new entry with the student in a new list
+        attendance_dict[day] = [student]
+    return attendance_dict
