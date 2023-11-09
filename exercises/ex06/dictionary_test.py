141b025f5a19459c486cef26936600f2a7497025
Zheyuan Liu <zheyuan@ad.unc.edu>
2023-11-08 19:11:49 -0500

    1

--- exercises/ex06/dictionary_test.py
+++ exercises/ex06/dictionary_test.py
@@ -2,70 +2,64 @@
 
 __author__ = "730521715"
 
-def test_invert_with_valid_input():
-    """Test invert function with a dictionary containing non-conflicting pairs."""
-    assert invert({'a': '1', 'b': '2'}) == {'1': 'a', '2': 'b'}
+import pytest
 
-def test_invert_with_non_string_values():
-    """Test invert function with non-string values."""
-    assert invert({1: 'a', 2: 'b'}) == {'a': 1, 'b': 2}
+# Assuming the functions are defined in a file named `functions.py` which is in the same directory.
+from functions import invert, favorite_color, count, alphabetize, update_attendance
 
-def test_invert_raises_key_error_on_duplicate_values():
-    """Test invert function raises KeyError on duplicate values."""
-    with pytest.raises(KeyError):
-        invert({'a': '1', 'b': '1'})
+# Unit tests for invert
+def test_invert_normal_case() -> None:
+    assert invert({'a': 1, 'b': 2, 'c': 3}) == {1: 'a', 2: 'b', 3: 'c'}
 
-def test_favorite_color_with_common_favorite():
-    """Test favorite_color returns the most common color."""
-    assert favorite_color({'Alice': 'blue', 'Bob': 'blue', 'Charlie': 'red'}) == 'blue'
+def test_invert_empty_dict() -> None:
+    assert invert({}) == {}
 
-def test_favorite_color_with_tie():
-    """Test favorite_color returns one of the most common colors in case of a tie."""
-    assert favorite_color({'Alice': 'blue', 'Bob': 'red', 'Charlie': 'red', 'David': 'blue'}) in ['blue', 'red']
+def test_invert_value_collision() -> None:
+    with pytest.raises(ValueError):
+        invert({'a': 1, 'b': 1})
 
-def test_favorite_color_with_empty_dict():
-    """Test favorite_color with an empty dictionary."""
-    assert favorite_color({}) is None
+# Unit tests for favorite_color
+def test_favorite_color_normal_case() -> None:
+    assert favorite_color({'Alice': 'blue', 'Bob': 'green', 'Charlie': 'blue'}) == 'blue'
 
-def test_count_with_valid_list():
-    """Test count function with a list of items."""
-    assert count(['a', 'b', 'a', 'c', 'b', 'a']) == {'a': 3, 'b': 2, 'c': 1}
+def test_favorite_color_empty_dict() -> None:
+    assert favorite_color({}) == None
 
-def test_count_with_empty_list():
-    """Test count function with an empty list."""
+def test_favorite_color_tie() -> None:
+    assert favorite_color({'Alice': 'blue', 'Bob': 'green', 'Charlie': 'green'}) in ['blue', 'green']
+
+# Unit tests for count
+def test_count_normal_case() -> None:
+    assert count(['red', 'blue', 'red', 'green', 'blue', 'blue']) == {'red': 2, 'blue': 3, 'green': 1}
+
+def test_count_empty_list() -> None:
     assert count([]) == {}
 
-def test_count_with_non_hashable_elements():
-    """Test count function with non-hashable elements in the list."""
-    with pytest.raises(TypeError):
-        count(['a', 'b', 'a', ['c', 'b'], 'a'])
+def test_count_single_element() -> None:
+    assert count(['red']) == {'red': 1}
 
-def test_alphabetize_with_unsorted_list():
-    """Test alphabetize function sorts the list alphabetically."""
-    assert alphabetize(['banana', 'apple', 'cherry']) == ['apple', 'banana', 'cherry']
+# Unit tests for alphabetizer
+def test_alphabetizer_normal_case() -> None:
+    assert alphabetize(['hello', 'apple', 'banana']) == ['apple', 'banana', 'hello']
 
-def test_alphabetize_with_already_sorted_list():
-    """Test alphabetize function with an already sorted list."""
-    assert alphabetize(['apple', 'banana', 'cherry']) == ['apple', 'banana', 'cherry']
-
-def test_alphabetize_with_empty_list():
-    """Test alphabetize function with an empty list."""
+def test_alphabetizer_empty_list() -> None:
     assert alphabetize([]) == []
 
-def test_update_attendance_with_present_student():
-    """Test update_attendance marks a present student correctly."""
-    attendance = {'Alice': 'absent', 'Bob': 'present'}
-    update_attendance(attendance, 'Alice')
-    assert attendance == {'Alice': 'present', 'Bob': 'present'}
+def test_alphabetizer_case_insensitivity() -> None:
+    assert alphabetize(['Hello', 'apple']) == ['apple', 'Hello']
 
-def test_update_attendance_with_all_students_absent():
-    """Test update_attendance with all students initially absent."""
-    attendance = {'Alice': 'absent', 'Bob': 'absent'}
-    update_attendance(attendance, 'Alice')
-    assert attendance == {'Alice': 'present', 'Bob': 'absent'}
+# Unit tests for update_attendance
+def test_update_attendance_normal_case() -> None:
+    attendance = {'Monday': ['Alice', 'Bob'], 'Wednesday': [], 'Friday': ['Alice', 'Charlie']}
+    update_attendance(attendance, 'Monday', 'Charlie')
+    assert attendance == {'Monday': ['Alice', 'Bob', 'Charlie'], 'Wednesday': [], 'Friday': ['Alice', 'Charlie']}
 
-def test_update_attendance_with_nonexistent_student():
-    """Test update_attendance with a student name not in the attendance."""
-    attendance = {'Alice': 'absent', 'Bob': 'present'}
-    with pytest.raises(KeyError):
-        update_attendance(attendance, 'Charlie')
+def test_update_attendance_new_day() -> None:
+    attendance = {}
+    update_attendance(attendance, 'Tuesday', 'Alice')
+    assert attendance == {'Tuesday': ['Alice']}
+
+def test_update_attendance_empty_case() -> None:
+    attendance = {'Monday': []}
+    update_attendance(attendance, 'Monday', 'Bob')
+    assert attendance == {'Monday': ['Bob']}
