9ebdbd30bc6dd45ad4f6d73a7ecfa4e4a64e0482
Zheyuan Liu <zheyuan@ad.unc.edu>
2023-11-08 19:33:04 -0500

    1

--- exercises/ex06/dictionary_test.py
+++ exercises/ex06/dictionary_test.py
@@ -1,82 +1,78 @@
-"""Dictionart_test."""
+"""Dictionary_test module: Unit tests for dictionary utility functions."""
 
 __author__ = "730521715"
 
-
 import pytest
-
-
-# Assuming the functions are defined in a file named `functions.py` which is in the same directory.
 from exercises.ex06.dictionary import invert, favorite_color, count, alphabetize, update_attendance
-
 
 # Unit tests for invert
 def test_invert_normal_case() -> None:
+    """Test inverting a dictionary with distinct key-value pairs."""
     assert invert({'a': 1, 'b': 2, 'c': 3}) == {1: 'a', 2: 'b', 3: 'c'}
 
-
 def test_invert_empty_dict() -> None:
+    """Test inverting an empty dictionary."""
     assert invert({}) == {}
 
-
 def test_invert_value_collision() -> None:
+    """Test inverting a dictionary with value collisions raises a ValueError."""
     with pytest.raises(ValueError):
         invert({'a': 1, 'b': 1})
 
-
 # Unit tests for favorite_color
 def test_favorite_color_normal_case() -> None:
+    """Test favorite_color with a clear winner color."""
     assert favorite_color({'Alice': 'blue', 'Bob': 'green', 'Charlie': 'blue'}) == 'blue'
 
-
 def test_favorite_color_empty_dict() -> None:
+    """Test favorite_color with an empty dictionary."""
     assert favorite_color({}) is None
 
-
 def test_favorite_color_tie() -> None:
+    """Test favorite_color when there is a tie in colors."""
     assert favorite_color({'Alice': 'blue', 'Bob': 'green', 'Charlie': 'green'}) in ['blue', 'green']
-
 
 # Unit tests for count
 def test_count_normal_case() -> None:
+    """Test counting elements in a list with multiple occurrences."""
     assert count(['red', 'blue', 'red', 'green', 'blue', 'blue']) == {'red': 2, 'blue': 3, 'green': 1}
 
-
 def test_count_empty_list() -> None:
+    """Test counting elements in an empty list."""
     assert count([]) == {}
 
-
 def test_count_single_element() -> None:
+    """Test counting elements in a list with a single element."""
     assert count(['red']) == {'red': 1}
 
-
-# Unit tests for alphabetizer
-def test_alphabetizer_normal_case() -> None:
+# Unit tests for alphabetize
+def test_alphabetize_normal_case() -> None:
+    """Test alphabetizing a list of strings."""
     assert alphabetize(['hello', 'apple', 'banana']) == ['apple', 'banana', 'hello']
 
-
-def test_alphabetizer_empty_list() -> None:
+def test_alphabetize_empty_list() -> None:
+    """Test alphabetizing an empty list."""
     assert alphabetize([]) == []
 
-
-def test_alphabetizer_case_insensitivity() -> None:
+def test_alphabetize_case_insensitivity() -> None:
+    """Test alphabetizing a list of strings with varying case."""
     assert alphabetize(['Hello', 'apple']) == ['apple', 'Hello']
-
 
 # Unit tests for update_attendance
 def test_update_attendance_normal_case() -> None:
+    """Test updating attendance with a new name on an existing day."""
     attendance = {'Monday': ['Alice', 'Bob'], 'Wednesday': [], 'Friday': ['Alice', 'Charlie']}
     update_attendance(attendance, 'Monday', 'Charlie')
     assert attendance == {'Monday': ['Alice', 'Bob', 'Charlie'], 'Wednesday': [], 'Friday': ['Alice', 'Charlie']}
 
-
 def test_update_attendance_new_day() -> None:
+    """Test updating attendance by adding a new day with a name."""
     attendance = {}
     update_attendance(attendance, 'Tuesday', 'Alice')
     assert attendance == {'Tuesday': ['Alice']}
 
-
 def test_update_attendance_empty_case() -> None:
+    """Test updating attendance with a new name on an empty day."""
     attendance = {'Monday': []}
     update_attendance(attendance, 'Monday', 'Bob')
     assert attendance == {'Monday': ['Bob']}
