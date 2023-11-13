3336717a3bcc2da8240f27d1260cfed42950c510
Zheyuan Liu <zheyuan@ad.unc.edu>
2023-11-13 15:58:05 -0500

    1

--- lessons/CQ07/point.py
+++ lessons/CQ07/point.py
@@ -3,6 +3,8 @@
 __author__ = "730521715"
 
 
+from typing import Union
+
 class Point:
     """Represents a point in a 2D space with x and y coordinates."""
 
@@ -11,11 +13,26 @@ class Point:
         self.x = x_init
         self.y = y_init
 
+
     def scale_by(self, factor: int) -> None:
         """Scale the point by a given factor in place."""
         self.x *= factor
         self.y *= factor
 
+
     def scale(self, factor: int) -> 'Point':
         """Scale the point by a given factor and return a new Point object."""
         return Point(self.x * factor, self.y * factor)
+    
+
+    def __str__(self) -> str:
+        """String representation of a Point."""
+        return f"x: {self.x}; y: {self.y}"
+
+    def __mul__(self, factor: Union[int, float]) -> 'Point':
+        """Multiply the point's coordinates by a factor."""
+        return Point(self.x * factor, self.y * factor)
+
+    def __add__(self, factor: Union[int, float]) -> 'Point':
+        """Add a value to the point's coordinates."""
+        return Point(self.x + factor, self.y + factor)
